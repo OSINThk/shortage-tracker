926fd0f529dd848488c693529d24936307a2f194
Nathan Hammond <github.com@nathanhammond.com>
2020-01-31 21:48:59 +0800

    Add basic validation of report contents.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,23 @@
 class Report < ApplicationRecord
+  validate :coordinates_valid
+  validates :user, presence: true
+
   belongs_to :user
+
+  private
+    def coordinates_valid
+      # Coordinate will be `nil` already if invalid.
+      # Error message is vague because it could be unentered, or invalid.
+      if coordinates.nil?
+        errors.add(:coordinates, "Please reenter location coordinate.")
+        return
+      end
+
+      # RGeo will happily parse a MULTIPOINT or any other valid WKT.
+      # Make sure we have a POINT.
+      if RGeo::Feature::Point != coordinates.geometry_type
+        errors.add(:coordinates, "Location coordinate must be a single point.")
+        return
+      end
+    end
 end
