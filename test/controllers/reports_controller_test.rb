17102cf311d64a03bc0b6eb6755bca47bd9e71f6
Nathan Hammond <github.com@nathanhammond.com>
2020-01-31 22:05:10 +0800

    Fix test failure and sign_in for tests.

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @report = reports(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test "should get index" do
