60fd86f48b8ed4acfd0be7075554b0ef8d403e32
Nathan Hammond <github.com@nathanhammond.com>
2020-01-31 16:04:08 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_183454) do
+ActiveRecord::Schema.define(version: 2020_01_31_080144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "privileges", force: :cascade do |t|
     t.string "name"
@@ -26,6 +27,16 @@ ActiveRecord::Schema.define(version: 2020_01_30_183454) do
     t.bigint "role_id", null: false
     t.index ["privilege_id", "role_id"], name: "index_privileges_roles_on_privilege_id_and_role_id"
     t.index ["role_id", "privilege_id"], name: "index_privileges_roles_on_role_id_and_privilege_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.inet "ip"
+    t.string "notes"
+    t.geography "coordinates", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -58,4 +69,5 @@ ActiveRecord::Schema.define(version: 2020_01_30_183454) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reports", "users"
 end
