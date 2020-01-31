b3285c4e764af9b989586bbb9d35aae1c9092e53
Nathan Hammond <github.com@nathanhammond.com>
2020-01-31 16:03:32 +0800

    Add detail to generated migration.

--- db/migrate/20200131080144_create_reports.rb
+++ db/migrate/20200131080144_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.0]
     create_table :reports do |t|
       t.inet :ip
       t.string :notes
-      t.st_point :coordinates
+      t.st_point :coordinates, geographic: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
