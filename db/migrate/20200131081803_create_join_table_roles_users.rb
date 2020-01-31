11b8a0450c483b92fd1f9c2aad667f561c9bf5b4
Nathan Hammond <github.com@nathanhammond.com>
2020-01-31 16:19:15 +0800

    rails g migration CreateJoinTableRolesUsers roles users

---
+++ db/migrate/20200131081803_create_join_table_roles_users.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableRolesUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_join_table :roles, :users do |t|
+      t.index [:role_id, :user_id]
+      t.index [:user_id, :role_id]
+    end
+  end
+end
