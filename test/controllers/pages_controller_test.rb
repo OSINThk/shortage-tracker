276f95c5d8fc088e8f312f59e0541f304c6d6422
Nathan Hammond <github.com@nathanhammond.com>
2020-02-02 01:42:49 +0800

    Makes Pages controller test pass.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
-    get pages_about_url
+    get about_url
+    assert_response :success
+  end
+
+  test "should get admin" do
+    get admin_url
     assert_response :success
   end
 
