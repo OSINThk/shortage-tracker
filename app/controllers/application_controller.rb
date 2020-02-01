3a39542475d29fa73c6abd37c8f4d7eab1a0c53a
Nathan Hammond <github.com@nathanhammond.com>
2020-02-01 12:55:27 +0800

    Add unique ID generation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+  private
+    def initialize
+      # Counter that is reset per request.
+      @unique_id = 0
+      super()
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_unique_id
+    @unique_id = @unique_id + 1
+    "unique-#{@unique_id}"
+  end
 end
