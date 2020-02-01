d34e3e7f4c59e1fdafff90225d7aa6930c172090
Nathan Hammond <github.com@nathanhammond.com>
2020-02-02 02:19:50 +0800

    Improve role and privilege presentation.

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -1,3 +1,7 @@
 class Privilege < ApplicationRecord
+  def to_s
+    return name
+  end
+
   has_and_belongs_to_many :role
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,8 @@
 class Role < ApplicationRecord
+  def to_s
+    return name
+  end
+
   has_and_belongs_to_many :privilege
   has_and_belongs_to_many :user
 end
