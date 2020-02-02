301a4e8f9a683999de4380acd16a5d75e44482b5
Keith Ng <keith@point.org.nz>
2020-02-03 01:18:01 +1300

    SECURITY: Sanitize inputs to avoid SQL injection vulnerability.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,19 +3,25 @@ class MapsController < ApplicationController
   end
 
   def results
-    @sql = "SELECT
-      p.id, p.name,
-      COUNT(*) AS live_reports,
-      MIN(price) AS min_price,
-      MAX(price) AS max_price,
-      MIN(scarcity) AS min_scarcity,
-      MAX(scarcity) AS max_scarcity
-    FROM reports AS r
-    INNER JOIN product_details AS d ON r.id=d.report_id
-    LEFT JOIN products AS p ON p.id=d.product_id
-    WHERE ST_Distance(coordinates, ST_Point(#{params[:lon]},#{params[:lat]})) < #{[params[:dist].to_i, 1000].max}
-    AND r.updated_at > '#{params[:since]}'
-    GROUP BY p.id, p.name;"
+    @sql = ActiveRecord::Base.send(:sanitize_sql_array, [
+      "SELECT
+        p.id, p.name,
+        COUNT(*) AS live_reports,
+        MIN(price) AS min_price,
+        MAX(price) AS max_price,
+        MIN(scarcity) AS min_scarcity,
+        MAX(scarcity) AS max_scarcity
+      FROM reports AS r
+      INNER JOIN product_details AS d ON r.id=d.report_id
+      LEFT JOIN products AS p ON p.id=d.product_id
+      WHERE ST_Distance(coordinates, ST_Point('%s','%s')) < %i
+      AND r.updated_at > '%s'
+      GROUP BY p.id, p.name;",
+      params[:lon],
+      params[:lat],
+      [params[:dist].to_i, 1000].max,
+      params[:since]
+    ])
     render json: ActiveRecord::Base.connection.execute(@sql)
   end
 end
