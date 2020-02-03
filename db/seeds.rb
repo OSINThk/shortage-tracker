5ca4a2c528206e2d3deabde7dff364756255e90b
Steven Buccini <sbuccini@apartmentlist.com>
2020-02-02 21:57:40 -0800

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = [
+  'masks',
+  'gloves',
+  'bottled water'
+].map { |name| Product.create(name: name) }
+
+products.each do |product|
+  product.product_detail.create(
+    price: rand(1..6),
+    scarcity: rand(1..5)
+  )
+end
+
+user = User.create
+
+
+coordiantes = [
+  '22.2276, 114.2178', # American club
+  '22.392998428 114.203999184', # horse racing
+  '22.274665568 114.155666044'
+
+]
+count = 0
+ProductDetail.all.with_index do |pd|
+  user.report.create(
+    prroduct_detail: pd,
+    coordinates: coordinates[count]
+  )
+  count += 1
+end
