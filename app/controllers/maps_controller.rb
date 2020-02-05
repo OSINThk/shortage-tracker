36c09d01515581694dfc0641713050195c7e64c7
Nathan Hammond <github.com@nathanhammond.com>
2020-02-05 10:08:45 +0800

    Implement parameter filtering for Maps#results.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,18 +1,47 @@
 require 'map_cursor'
+require 'bigdecimal'
 
 class MapsController < ApplicationController
   def index
   end
 
   def results
-    root = params["root"]
-    parent_cursors = params["cursors"];
+    params.permit(:format, :x0, :y0, :x1, :y1, :root, :cursors => [], :products => [])
+
+    # BAD: ?root, ?root=, ?root=garbage, ?root[], ?root[]=, ?root[]=garbage
+    # GOOD: ?root=1234567
+    if params["root"].is_a?(String) && params["root"][/^\h{7}$/]
+      root = params["root"]
+    end
+
+    # BAD: ?cursors, ?cursors=, ?cursors=garbage, ?cursors[], ?cursors[]=
+    # BAD: ?cursors[]=garbage&cursors[]=garbage, ?cursors[]=aaaaaaa&cursors[]=aaaaaaa
+    # GOOD: ?cursors[]=1234567&cursors[]=890abcd
+    if params["cursors"].is_a?(Array)
+      parent_cursors = params["cursors"]
+      parent_cursors.reject! {|cursor| !cursor.is_a?(String) || !cursor[/^\h{7}$/] }
+      parent_cursors.uniq!
+    end
+
+    # BAD: ?products, ?products=, ?products=garbage, ?products[], ?products[]=
+    # BAD: ?products[]=garbage&products[]=garbage, ?products[]=aaaaaaa&products[]=aaaaaaa
+    # GOOD: ?products[]=1&products[]=2
+    if params["products"].is_a?(Array)
+      products = params["products"]
+      products.reject! {|product| !product.is_a?(String) || !product[/^\d+$/] }
+      products.uniq!
+    end
+
+    x0 = float_between(params["x0"], 180)
+    y0 = float_between(params["y0"], 90)
+    x1 = float_between(params["x1"], 180)
+    y1 = float_between(params["y1"], 90)
+
+    if (x0.nil? || y0.nil? || x1.nil? || y1.nil?)
+      raise ActionController::ParameterMissing.new("You must provide valid coordinates.")
+    end
+
     time = DateTime.now
-    x0 = params["x0"];
-    y0 = params["y0"];
-    x1 = params["x1"];
-    y1 = params["y1"];
-    products = params["products"]
 
     cursor = MapCursor.new(parent_cursors, time, x0, y0, x1, y1, products)
     @results = cursor.query(root)
@@ -41,4 +70,28 @@ class MapsController < ApplicationController
     # ])
     # @results = ActiveRecord::Base.connection.execute(@sql)
   end
+
+  private
+    def float_between(possible_string, bound)
+      # Is a string.
+      if !possible_string.is_a?(String)
+        return nil
+      else
+        as_string = possible_string
+      end
+
+      # Matches a string representation of a float.
+      if !as_string[/^[-+]?[0-9]*\.?[0-9]+$/]
+        return nil
+      else
+        as_float = as_string.to_f
+      end
+
+      # Within bounds.
+      if as_float >= -bound.to_f && as_float <= bound.to_f
+        return as_float
+      else
+        return nil
+      end
+    end
 end
