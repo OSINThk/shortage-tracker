2f9b82fdc8a7d100483757e66980a14b85201e87
Nathan Hammond <github.com@nathanhammond.com>
2020-02-07 01:29:42 +0800

    Make db reads lazy.

--- config/initializers/maxmind.rb
+++ config/initializers/maxmind.rb
@@ -1,4 +1,5 @@
 require 'maxmind/db'
+require 'geo_ip'
 
-$geoip_city = MaxMind::DB.new('GeoLite2-City.mmdb', mode: MaxMind::DB::MODE_MEMORY)
-$geoip_asn = MaxMind::DB.new('GeoLite2-ASN.mmdb', mode: MaxMind::DB::MODE_MEMORY)
+$geoip_city = GeoIp.new('City')
+$geoip_asn = GeoIp.new('ASN')
---
+++ lib/geo_ip.rb
@@ -0,0 +1,18 @@
+class GeoIp
+  def initialize(type)
+    @current = nil
+    @type = type
+  end
+
+  def get(*args)
+    if @current.nil?
+      setup()
+    end
+
+    @current.get(*args)
+  end
+
+  def setup
+    @current = MaxMind::DB.new("GeoLite2-#{@type}.mmdb", mode: MaxMind::DB::MODE_MEMORY)
+  end
+end
