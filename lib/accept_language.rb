5cbfa98acc7d830b0e8543ef2f7981b2e33cc12a
Nathan Hammond <github.com@nathanhammond.com>
2020-02-07 19:07:42 +0800

    Implement Accept-Language header parsing.

---
+++ lib/accept_language.rb
@@ -0,0 +1,84 @@
+require 'locale'
+
+PIECES = /(?:^|,| )([A-Za-z\-\*]+)(?:(?:;q=)?(\.[0-9]+|0\.[0-9]+|0|1))?(?=$|,| )/
+
+module Enumerable
+  def stable_sort
+    sort_by.with_index { |x, idx| [x, idx] }
+  end
+
+  def stable_sort_by
+    sort_by.with_index { |x, idx| [yield(x), idx] }
+  end
+end
+
+# Usage
+# require 'accept_language'
+# header = AcceptLanguage::Header.parse(request.env['HTTP_ACCEPT_LANGUAGE'])
+
+module AcceptLanguage
+  class Value
+    attr_reader :quality
+    attr_reader :locale
+
+    def initialize(locale, quality)
+      if locale != "*"
+        @locale = Locale.from_rfc5646(locale)
+      else
+        @locale = "*"
+      end
+
+      quality = quality.nil? ? 1.0 : quality.to_f
+
+      if quality > 1
+        quality = 1.0
+      end
+
+      if quality < 0
+        quality = 0.0
+      end
+
+      @quality = quality
+    end
+
+    def <=>(other)
+      case other
+        when Value
+          if @quality < other.quality
+            return 1
+          end
+          if @quality == other.quality
+            return 0
+          end
+          if @quality > other.quality
+            return -1
+          end
+        else
+          nil
+      end
+    end
+
+    def to_s
+      output = @locale.is_a?(Locale) ? @locale.rfc5646 : @locale
+      return "#{output};q=#{@quality}"
+    end
+  end
+
+  class Header
+    attr_reader :values
+
+    def initialize(values)
+      @values = values.stable_sort()
+    end
+
+    def self.parse(header_string)
+      values = []
+      header_string.scan(PIECES).each do |match|
+        locale, quality = match
+        values << Value.new(locale, quality)
+      end
+
+      return Header.new(values)
+    end
+  end
+end
