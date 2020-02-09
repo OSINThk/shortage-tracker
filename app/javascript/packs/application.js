2b80110977ef44ef4c0d8da804530f7c1cfa5261
thehapax <39811582+thehapax@users.noreply.github.com>
2020-02-08 18:30:35 -0800

    add webpack bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ require("../manage-relationships");
 import "bootstrap"
 import "../stylesheets/application"
 
-
 //= require lodash
 
 // Uncomment to copy all static images under ../images to the output folder and reference
